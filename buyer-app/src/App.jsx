39e9f0621b2c1b327a533148e86b1adbac12f158
SriHarsithaNagarajan2803 <dnsriharsitha@gmail.com>
2026-08-28 13:12:58 +0530

    Initial commit: HomePot base setup

---
+++ buyer-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-orange-50 text-orange-600 font-bold text-2xl">
+      🍲 HomePot Buyer App Running!
+    </div>
+  );
+}
